7025ca9b51d97b5c22a2d99b08a7c0626463e84f
samtari1 <samtariyang@gmail.com>
2026-03-15 20:58:27 -0400

    Initial Commit

--- ImageAnalysis/ContentView.swift
+++ ImageAnalysis/ContentView.swift
@@ -1,24 +1,47 @@
-//
-//  ContentView.swift
-//  ImageAnalysis
-//
-//  Created by Quanpeng Yang on 3/15/26.
-//
-
 import SwiftUI
+import Vision
 
 struct ContentView: View {
+
+    @State private var categoriesList: String = "Loading..."
+
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Image("picture1")
+                .resizable()
+                .scaledToFit()
+                .padding()
+
+            Text(categoriesList)
+
+            Spacer()
         }
         .padding()
-    }
-}
+        .task {
+            guard let uiImage = UIImage(named: "picture1"),
+                  let cgImage = uiImage.cgImage else {
+                categoriesList = "Image not found or cannot convert to CGImage!"
+                return
+            }
 
-#Preview {
-    ContentView()
+            do {
+                let request = ClassifyImageRequest()
+                var results = try await request.perform(on: cgImage)
+                
+                results = results.filter {
+                    $0.hasMinimumPrecision(0.1, forRecall: 0.8) && $0.confidence > 0.1
+                }
+
+                categoriesList = results.isEmpty
+                    ? "No categories found"
+                    : results.map(\.identifier).joined(separator: ", ")
+
+            } catch {
+                categoriesList = "Error: \(error)"
+                print(error)
+            }
+        }
+    }
 }
